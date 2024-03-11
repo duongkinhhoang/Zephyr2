60ce609f1360cdb9bde667b33f8cfc5cfd862e78
duongkinhhoang <82352379+duongkinhhoang@users.noreply.github.com>
2024-03-12 00:10:21 +0700

    Update Runner.java

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -7,7 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/resources/features/OrangeAutomationTest.feature"},
-        glue = "stepDefinitions"
+        glue = "stepDefinitions",
+        plugin = {"junit:target/cucumber/result.xml", "json:target/cucumber/calculator.json"}
 )
 public class Runner {
 }
